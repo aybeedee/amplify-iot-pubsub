20a495abb28c1eb9fddd99b57bfce5054b983d03
Abdullah Umer <abdullahumer575@gmail.com>
2023-07-16 18:20:00 +0500

    added auth and pubsub

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "amplifyiotpubsub9c1509b2": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import './App.css';
+import { Amplify, PubSub, Hub } from 'aws-amplify';
+import { AWSIoTProvider, CONNECTION_STATE_CHANGE } from '@aws-amplify/pubsub';
+import config from './aws-exports';
+import "@aws-amplify/ui-react/styles.css";
+import { withAuthenticator } from "@aws-amplify/ui-react";
 
-function App() {
-  const [count, setCount] = useState(0)
+Amplify.configure(config);
+
+const REGION = process.env.IOT_REGION_ENV;
+const ENDPOINT = process.env.IOT_ENDPOINT_ENV;
+
+// Apply plugin with configuration
+Amplify.addPluggable(
+  new AWSIoTProvider({
+    aws_pubsub_region: REGION,
+    aws_pubsub_endpoint: ENDPOINT
+  })
+);
+
+function App({ signOut }) {
+
+  async function publishToTopic() {
+    try {
+      await PubSub.publish("executeTopic", {
+        command: "run"
+      });
+      console.log("sent run command!");
+    } catch(err) {
+      console.log(err)
+    }
+  }
+
+  useEffect(() => {
+
+    console.log(REGION, ENDPOINT)
+
+    PubSub.subscribe('dataTopic').subscribe({
+      next: data => console.log('Message received', data),
+      error: error => console.error(error),
+      complete: () => console.log('Done')
+    });
+
+    Hub.listen('dataTopic', (data) => {
+      const { payload } = data;
+      if (payload.event === CONNECTION_STATE_CHANGE) {
+        const connectionState = payload.data.connectionState;
+        console.log(connectionState);
+      }
+    });
+  }, [])
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h3>Amplify x IoT Core</h3>
+      <button onClick ={publishToTopic}>Run Simulator</button>
+      <button onClick={signOut}>Sign Out</button>
     </>
-  )
+  );
 }
 
-export default App
+export default withAuthenticator(App);
